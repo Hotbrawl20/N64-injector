1a066050368e827b3209d41c0645806325e32ef2
NicoAICP <nicohaberkorn@gmx.de>
2017-10-11 15:47:41 +0200

    Added update choice

--- N64Injector.bat
+++ N64Injector.bat
@@ -1,4 +1,14 @@
+@echo off
+echo Run N64 VC Injector (1)
+echo Run Updater (2)
+set /p CHOICE=What do you want to do: 
+if %CHOICE%==1 goto runinjector
+if %CHOICE%==2 goto runupdater
+:runinjector
 cd Tools
 cd N64
 start N64INJECTOR.bat
+exit
+:runupdater
+start Updater.bat
 exit
